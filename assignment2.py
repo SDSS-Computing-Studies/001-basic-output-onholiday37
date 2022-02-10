25ded67746202f0be7622cc5e75d66d367bf3316
onholiday37 <99277588+onholiday37@users.noreply.github.com>
2022-02-10 09:30:11 -0800

    I m done

---
+++ assignment2.py
@@ -0,0 +1,23 @@
+##### Task 2
+"""
+
+(2 points) **Create** a new assignment file called "assignment2.py".  It should display the following:
+
+Sample Output Task #2
+This is my second program.
+It uses "two commands" to display the output.
+
+Note that you can't use the command print("It uses "two commands" to display the output.") because the Python3 interpreter sees the second double quotation is ending the first one.  In this case, we have two choices:
+  - Use a single quotation mark as the beginning and end of the output string.
+  - "Escape" the internal double quotation mark.  Using \" instead of " will tell the compiler that the double quote is for display, and is not functional
+
+"""
+"""
+print("Sample Output Task #2\nThis is my second program.\nIt uses \"two commands\" to display the output.")
+
+print("Sample Output Task #2\nThis is my second program.\nIt uses 'two commands' to display the output.")
+
+this is actually the right anwser but I will just leave it there.
+"""
+print("Sample Output Task #2.\nThis is my second program.")
+print("It uses \"two commands\" to display the output.")
